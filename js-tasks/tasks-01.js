023300430e64b86f7c7ea4114e4a03fd39d5bdfa
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2023-10-16 20:16:31 +0300

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -10,3 +10,29 @@ function evenOrOdd(number) {
     return "Odd";
   }
 }
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+  // Треба перевірити число на парність
+  if (number % 2 === 0) {
+    // Якщо парне - повернути "Even"
+    return "Even";
+  } else {
+    // Якщо непарне - повернути "Odd"
+    return "Odd";
+  }
+}
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+  // Треба перевірити число на парність
+  if (number % 2 === 0) {
+    // Якщо парне - повернути "Even"
+    return "Even";
+  } else {
+    // Якщо непарне - повернути "Odd"
+    return "Odd";
+  }
+}
