64b3ae3f4fa7351dc0e6cc76bdb3259bbc9cbdb8
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2023-10-16 20:14:16 +0300

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+  // Треба перевірити число на парність
+  if (number % 2 === 0) {
+    // Якщо парне - повернути "Even"
+    return "Even";
+  } else {
+    // Якщо непарне - повернути "Odd"
+    return "Odd";
+  }
+}
