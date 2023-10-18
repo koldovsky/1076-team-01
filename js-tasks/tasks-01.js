4b66e10b7f73aecf6a673a28a9a2b9b9c4f7c3a3
Anton Skochulyas <antonskochulyas@gmail.com>
2023-10-18 16:29:08 +0300

    Update tasks-01.js
    Add codewars answers to file. Team 1.

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,38 +1,49 @@
-// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+// 1. Messi's goals function:
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
-function evenOrOdd(number) {
-  // Треба перевірити число на парність
-  if (number % 2 === 0) {
-    // Якщо парне - повернути "Even"
-    return "Even";
-  } else {
-    // Якщо непарне - повернути "Odd"
-    return "Odd";
-  }
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+// ----------------------------------------------------------------------------------------------------------------
 
-function evenOrOdd(number) {
-  // Треба перевірити число на парність
-  if (number % 2 === 0) {
-    // Якщо парне - повернути "Even"
-    return "Even";
-  } else {
-    // Якщо непарне - повернути "Odd"
-    return "Odd";
-  }
+// 2. Return Negative:
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+const makeNegative = (num) => (num >= 0 ? num * -1 : num);
+
+// ----------------------------------------------------------------------------------------------------------------
+
+// 3. Terminal game move function:
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move(position, roll) {
+  return position + roll * 2;
 }
 
-// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+// ----------------------------------------------------------------------------------------------------------------
 
-function evenOrOdd(number) {
-  // Треба перевірити число на парність
-  if (number % 2 === 0) {
-    // Якщо парне - повернути "Even"
-    return "Even";
-  } else {
-    // Якщо непарне - повернути "Odd"
-    return "Odd";
-  }
+// 4. Personalized Message:
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
 }
+
+// ----------------------------------------------------------------------------------------------------------------
+
+// 5. Keep Hydrated!:
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+// ----------------------------------------------------------------------------------------------------------------
+
+// 6. Opposites Attract:
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+const lovefunc = (flower1, flower2) => (flower1 + flower2) % 2 === 1;
+
+// ----------------------------------------------------------------------------------------------------------------
