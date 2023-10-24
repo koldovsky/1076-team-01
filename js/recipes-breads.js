090a7bbbbe96091586cce6fc680a88e93e438ef6
Serhii-Vivdenko <131198995+Serhii-Vivdenko@users.noreply.github.com>
2023-10-24 14:07:37 +0000

    update file

--- js/recipes-breads.js
+++ js/recipes-breads.js
@@ -1,1 +1,15 @@
 const title = document.querySelector('.recipes-breads__title');
+const changeTitle = ['Recipes Using Our Breads', 'The best recipe',];
+let currentIndex = 0;
+
+function changeTitleText() {
+  title.textContent = changeTitle[currentIndex];
+  currentIndex = currentIndex + 1;
+  if(currentIndex === changeTitle.length){
+    currentIndex = 0;
+  }
+}
+
+setInterval(changeTitleText, 2000);
+
+
