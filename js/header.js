91f2b7be389a274983b6a3ec434bf1c58bb983ef
Nadiia <108687902+NadezhdaKuzmich@users.noreply.github.com>
2023-10-25 14:46:37 +0000

    edit js files

--- js/header.js
+++ js/header.js
@@ -4,13 +4,8 @@ const btnSub = document.querySelector(".header__menu-btn");
 const navLinks = document.querySelector(".header__nav-menu");
 
 btnSub.addEventListener("click", () => {
-    if (header.className === "header") {
-        body.classList.add("fixed");
-        header.classList.add("responsive");
-    } else {
-        header.classList.remove("responsive");
-        body.classList.remove("fixed");
-    }
+    body.classList.toggle("fixed");
+    header.classList.toggle("responsive");
 })
 
 navLinks.addEventListener("click", () => {
