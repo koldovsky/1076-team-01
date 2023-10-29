ff537beb1f1577162bdae87f73840562181888d9
Anton Skochulyas <antonskochulyas@gmail.com>
2023-10-29 12:36:42 +0000

    add js to about us block, add clients count, add css to clients count

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("partialsLoaded", () => {
   import("./header.js");
   import("./recipes-breads.js");
+  import("./about-us-our-clients.js");
   import("./handcrafted.js");
   import("./locations-cards.partial.js");
 });
