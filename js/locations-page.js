1726ac235564f019d6d0bc047c01871466d14161
Anton <89017124+antonbaider@users.noreply.github.com>
2023-10-30 15:24:43 +0000

    improve js css form

--- js/locations-page.js
+++ js/locations-page.js
@@ -1,5 +1,6 @@
 document.addEventListener("partialsLoaded", () => {
   import("./header.js");
   import("./form-validation.js");
+  import("./contact-form-formspree.js");
   import("./locations-cards.partial.js");
 });
