aa56b616f7f8ece168d77f76534e948094dc6d6b
Serhii Voropaiev <120410659+SerhiiVoropaiev@users.noreply.github.com>
2023-11-09 12:24:20 +0000

    added hover footer/social medias icons

--- js/index.js
+++ js/index.js
@@ -7,4 +7,5 @@ document.addEventListener("partialsLoaded", () => {
   import("./about-us-our-clients.js");
   import("./recipes-breads.js");
   import("./locations-cards.partial.js");
+  import("./footer-partial.js");
 });
